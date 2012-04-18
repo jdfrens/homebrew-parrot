70d91bcece445a07d410aed120123ba54c979c58
Jeremy D. Frens <jdfrens@gmail.com>
2012-04-18 09:35:14 -0500

    Fix bug: rename class to match file

--- parrot-devel.rb
+++ parrot-devel.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Parrot < Formula
+class ParrotDevel < Formula
   head 'https://github.com/parrot/parrot.git'
   url 'ftp://ftp.parrot.org/pub/parrot/releases/stable/4.3.0/parrot-4.3.0.tar.bz2'
   sha256 '5c3f5ba2de06f6adb53b7835374a4f3e0601ec63e8a1d1dba6c6a07e12cc2990'
