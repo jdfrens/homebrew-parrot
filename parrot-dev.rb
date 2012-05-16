5db72b3da1fa15a06d1e201c852166d7d0b63e82
Jeremy D. Frens <jdfrens@gmail.com>
2012-05-16 08:58:59 -0500

    Announcement lied; new version is devel, not stable

--- parrot-dev.rb
+++ parrot-dev.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ParrotDev < Formula
   head 'https://github.com/parrot/parrot.git'
-  url 'ftp://ftp.parrot.org/pub/parrot/releases/stable/4.4.0/parrot-4.4.0.tar.bz2'
+  url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/4.4.0/parrot-4.4.0.tar.bz2'
   sha256 '348ce13fc136afc74a7b50b094f64d8cb00f83f0cd3d59acc6fa4e63c824fa4d'
   homepage 'http://www.parrot.org/'
 
